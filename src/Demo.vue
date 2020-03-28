be4abf2b5fa5fbfff6b332997ebcde2894b61583
Herson Salinas <hersonhn@gmail.com>
2020-03-27 23:51:13 -0600

    vue component

---
+++ src/Demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="luna-demo">
+    <luna-canvas theme="light" />
+    <luna-canvas theme="dark" />
+  </div>
+</template>
+
+<script>
+import LunaCanvas from './components/LunaCanvas.vue';
+
+export default {
+  name: 'Demo',
+  components: {
+    LunaCanvas,
+  },
+};
+</script>
+
+<style>
+.luna-demo {
+  margin-top: 60px;
+  text-align: center;
+  background: lightgray;
+  padding: 10px;
+}
+</style>
---
+++ src/components/LunaCanvas.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="luna-canvas"
+    :class="[theme]"
+  >
+    <canvas class="luna" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LunaCanvas',
+  props: {
+    theme: {
+      type: String,
+      default: 'light',
+    },
+  },
+  data() {
+    return {
+
+    };
+  },
+
+  created() {
+  },
+};
+</script>
+
+<style scoped>
+  .luna-canvas {
+    display: inline-block;
+  }
+
+  .luna-canvas.light { background: white; }
+  .luna-canvas.dark { background: black; }
+
+  canvas.luna {
+    display: block;
+    width: 300px;
+    height: 300px;
+  }
+</style>
